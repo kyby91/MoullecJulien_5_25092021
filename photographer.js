10d399132d390dbb69c3241e9ccdb2188340eaeb
Julien Moullec <julienmoullec@yahoo.fr>
2021-12-30 19:39:52 +0100

    pb affichage photo

--- photographer.js
+++ photographer.js
@@ -3,9 +3,8 @@
 const firstDiv = document.querySelector('main div')
 const urlParams = new URLSearchParams(window.location.search)
 const urlId = urlParams.get("id")
-const secDiv = document.getElementsByClassName('Photos')
+const secDiv = document.getElementById('Photos')
 
-console.log(urlId)
 
 fetch('data.json').then(response => {
     return response.json();
@@ -15,8 +14,6 @@ fetch('data.json').then(response => {
 
     
     const photograph = photographers.find(photographe => photographe.id == urlId)
-    console.log(photograph)
-    console.log(photograph.name)
 
     let div1 = document.createElement('div')
     div1.setAttribute('id', 'photograph-info')
@@ -43,95 +40,78 @@ fetch('data.json').then(response => {
     div2.appendChild(profilepicture)
 
 
-    console.log(profilepicture)
-    console.log(firstDiv)
 
     //affichage de ses photos
 
     const photos = data.media
 
     let match = photos.filter( photo => photo.photographerId == urlId )
-    console.log(match)
 
     const facto = factoryMediaElt()
     match.forEach( item => {
         // Element image or vidéo
-        console.log(facto.choiceElt(item) )
-
+        facto.choiceElt(item)
         // Conception de la suite de HTML (Carte media)
+        let div3 = document.createElement('div')
+        secDiv.appendChild(div3)
+        console.log(secDiv);
+
+        let description = document.createElement('p')
+        description.innerHTML = item.title
+
         //Appendchilm
+        div3.appendChild(description)
+        
     })
 
-    
 
-    //->Chercher le bon photographe
-        //Chercher dans mon tableau photographe celui qui a l'id = urlId
-        //Un photographe (Le bon)
-        //-> Afficher les données du photographe dans la page
-
-    //->Chercher ses medias (photo et video)
-        //Chercher dans mon tableau media , ceux qui ont l'id = urlId
-        //Recrer un tableau de média tu photograpge en question
-        //Afficher ses medias
-            //Factory methode (créer lement html image ou video)
 
 
 }).catch(error=> {
     console.error(error)
 })
 
+"/SamplePhotos/Ellie Rose/Architecture_Connected_Curves.jpg"
+"\SamplePhotos\Nabeel\Travel_On_the_Road.jpg"
 
 
-function affichagePhotos(photos) {
-    photos.forEach(photo =>{
-        let picture = document.createElement('img')
-        picture.src = '/SamplePhotos/'+ photograph.name + '/' + photo.image
-        secDiv.appendChild(picture)
-    })
-}
 
 
 function factoryMediaElt() {
 
+
     function eltImage(data){
         //Création délemment image HTML
+        let picture = document.createElement('img')
+        picture.src = 'SamplePhotos/' + data.image
+        return data;
+    }
+
+    function eltVideo(data){
+        let video = document.createElement('video')
+        video.src = 'SamplePhotos/' + data.video
         return data;
     }
 
 
     function choiceElt(data){
-        console.log(data.image)
         let elt;
         if(data.image){   
             // elt = 'image'
-            elt = this.eltImage('image')
-            
+            elt = this.eltImage(data)
         }else{
-            elt = 'vidéo'
+            elt = this.eltVideo(data)
         }
-
         return elt;
 
         // console.log(elt)
         // If image ou vidéo (HTML)
     }
 
-
     return {
         eltImage,
+        eltVideo,
         choiceElt
     }
 
 }
-
-
-
-
-const object = {
-    name : "thomas",
-    myFonction() {
-        console.log(this.name)
-    }
-}
-
-object.myFonction()
